8027d55d79178050fbcfe2dbbdd8a4921129937a
raniw <yassinebouazzati2@gmail.com>
2024-10-17 10:55:52 +0200

    Test qui ne passe pas

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -2,6 +2,7 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.Null;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -9,29 +10,26 @@ import static org.mockito.Mockito.*;
 public class IPokemonFactoryTest {
 
     private IPokemonFactory pokemonFactory;
+    private IPokemonMetadataProvider pokemonMetaDataProvider;
+    private PokemonMetadata pokemonMetadata;
     private Pokemon bulbasaur;
 
     @Before
-    public void setUp() {
+    public void setUp() throws PokedexException {
         // Création d'un mock pour IPokemonFactory
         pokemonFactory = mock(IPokemonFactory.class);
+        pokemonMetaDataProvider = mock(IPokemonMetadataProvider.class);
 
-        // Création de deux instances de Pokemon pour les tests
-        bulbasaur = new Pokemon(
-                0, // Index
-                "Bulbizarre", // Nom
-                126, // Attaque
-                126, // Défense
-                90, // Endurance
-                613, // CP
-                64, // HP
-                4000, // Poussière d'étoile
-                4, // Bonbons
-                56.0 // IV
-        );
+        pokemonMetadata = new PokemonMetadata(0,"Bulbizarre",117,117,81);
+
+        bulbasaur = new Pokemon(0,"Bulbizarre", 126, 126, 90, 613,
+                64, 4000,4,56.0);
 
         // Configuration du comportement du mock pour la méthode createPokemon
         when(pokemonFactory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(bulbasaur);
+        when(pokemonFactory.createPokemon(-1, 613, 64, 4000, 4)).thenReturn(null);
+        when(pokemonFactory.createPokemon(151, 613, 64, 4000, 4)).thenReturn(null);
+        when(pokemonMetaDataProvider.getPokemonMetadata(0)).thenReturn(pokemonMetadata);
     }
 
     @Test
@@ -52,15 +50,41 @@ public class IPokemonFactoryTest {
 
     }
 
-    /*@Test(expected = IllegalArgumentException.class)
+    @Test
     public void testCreatePokemonWithInvalidIndexTooLow() {
         // Essaie de créer un Pokémon avec un index invalide (trop bas)
-        pokemonFactory.createPokemon(-1, 613, 64, 4000, 4);
+        assertNull(pokemonFactory.createPokemon(-1, 613, 64, 4000, 4));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testCreatePokemonWithInvalidIndexTooHigh() {
         // Essaie de créer un Pokémon avec un index invalide (trop haut)
-        pokemonFactory.createPokemon(151, 613, 64, 4000, 4);
-    }*/
+        assertNull(pokemonFactory.createPokemon(151, 613, 64, 4000, 4));
+    }
+
+    @Test
+    public void testPokemonMetaDataBetween0And15() {
+        Pokemon createdBulbasaur = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+
+        assertTrue("La valeur d'attaque bonus est supérieure à 15", (createdBulbasaur.getAttack() -pokemonMetadata.getAttack()) <= 15);
+        assertTrue("La valeur de défense bonus est supérieure à 15", (createdBulbasaur.getDefense() - pokemonMetadata.getDefense()) <= 15);
+        assertTrue("La valeur d'endurance bonus est supérieure à 15", (createdBulbasaur.getStamina() - pokemonMetadata.getStamina()) <= 15);
+
+        assertTrue("La valeur d'attaque bonus est inférieur à 0", (createdBulbasaur.getAttack() - pokemonMetadata.getAttack()) >= 0);
+        assertTrue("La valeur de défense bonus est inférieur à 0", (createdBulbasaur.getDefense() - pokemonMetadata.getDefense()) >= 0);
+        assertTrue("La valeur d'endurance bonus est inférieur à 0", (createdBulbasaur.getStamina() - pokemonMetadata.getStamina()) >= 0);
+    }
+
+    @Test
+    public void testPokemonMetaDataCoherent() {
+        //Vérifie que le calcul des bonus est cohérent entre l'IV du pokemon et la stat de base de l'espèce
+        Pokemon createdBulbasaur = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+
+        int valeurBonus = (int) Math.ceil((createdBulbasaur.getIv() / 100) * 15);
+
+        assertEquals(createdBulbasaur.getAttack(), pokemonMetadata.getAttack() + valeurBonus);
+        assertEquals(createdBulbasaur.getDefense(), pokemonMetadata.getDefense() + valeurBonus);
+        assertEquals(createdBulbasaur.getStamina(), pokemonMetadata.getStamina() - valeurBonus);
+    }
+
 }
